6295f6a2538e623816ec8dfa832346dca2fb174a
vvmakarov <github@bazilio.ru>
2021-05-27 14:32:04 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+## A highly specialized function library
+
+### Not of interest to third party users
+
---
+++ test/echo/isLevelAllowed.spec.js
@@ -0,0 +1,61 @@
+// const config = require('config');
+const { expect } = require('chai');
+const echo = require('../../index');
+
+// const currentLevel = config.get('logger.level');
+// const { defaultLevel } = echo;
+
+function s (str) {
+    return `${typeof str === 'string' ? `"${str}"` : `${str}`}`;
+}
+
+describe('echo.isLevelAllowed() should work properly', () => {
+    describe(`isLevelAllowed (current = ${echo.level})`, () => {
+        const testArr = [
+            ['error', true],
+            ['warn', true],
+            ['info', true],
+            ['verbose', false],
+            ['VERBOSE', false],
+            ['debug', false],
+            ['silly', false],
+            ['SILLY', false],
+            ['0', true],
+            [1, true],
+            ['2', true],
+            [3, false],
+            [4, false],
+            ['4', false],
+            ['', true],
+            ['-2', true],
+            [-2, true],
+            [8, true],
+            ['foo', true]
+        ];
+        testArr.forEach(([testLevel, bExprcted]) => {
+            it(`${s(testLevel)} -> ${echo.level} - ${bExprcted}`, () => {
+                const bResult = echo.isLevelAllowed(testLevel);
+                expect(bResult).to.equal(bExprcted);
+            });
+        });
+    });
+    describe('isLevelAllowed', () => {
+        const testArr = [
+            ['error', 'warn', true],
+            ['warn', 'error', false],
+            ['info', 'VERBOSE', true],
+            ['verbose', 'SILLY', true],
+            ['verbose', 'foo', false],
+            ['silly', 'foo', false],
+            ['foo', 'silly', true],
+            ['foo', 'info', true],
+            ['foo', 'warn', false]
+        ];
+        testArr.forEach(([testLevel, compareLevel, bExprcted]) => {
+            const bResult = echo.isLevelAllowed(testLevel, compareLevel);
+            it(`${s(testLevel)} -> ${s(compareLevel)} - ${bResult}`, () => {
+                expect(bResult).to.equal(bExprcted);
+            });
+        });
+    });
+});
